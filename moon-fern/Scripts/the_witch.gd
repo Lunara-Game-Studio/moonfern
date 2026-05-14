ac48fefee762b52548279f3bed71726de0aedefb
OlivGlov <glovolivia@gmail.com>
2026-05-13 22:00:51 -0700

    Pick up system

--- moon-fern/Scripts/the_witch.gd
+++ moon-fern/Scripts/the_witch.gd
@@ -2,9 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 600.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -600.0
+const MAX_ITEMS = 1 # we don't want the player to be able to carry more
 
-
+var full_inventory = false
+var inventory = []
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -24,3 +26,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
+	
+	
+# Adding items to inventory
+# Max one item in inventory
+func add_item(item_name):
+	if inventory.size() >= MAX_ITEMS:
+		return
+	inventory.append(item_name)
+	full_inventory = true
+	print(inventory) # for debugging
